82c34e75e7432234e2fff9376b2244fb5895f7a6
unknown <nahin.pablo@gmail.com>
2020-09-02 23:34:28 -0300

    estileo referencias a pleno

--- src/map-component/legend.js
+++ src/map-component/legend.js
@@ -11,9 +11,6 @@ class Legend extends React.Component {
 
     //no creo que sean propiedades del componente...
     this.state = {
-        arduinos: [
-            {latitude: -34.590319, longitude: -58.382822},
-          ]
     }  
   }
 
@@ -25,25 +22,25 @@ class Legend extends React.Component {
         <div
         
         style={legendStyle}>
-            <h3>Referencias</h3>
+            <h3>REFERENCIAS</h3>
             <div
             style={referenceStyle}>
-            <img src={sensorIcon}/>
+            <img src={sensorIcon} width={"32"} height={"32"}/>
             Ocupado
             </div>
             <div
             style={referenceStyle}>
-            <img src={motoIcon}/>
+            <img src={motoIcon} width={"32"} height={"32"}/>
             Motos
             </div>
             <div
             style={referenceStyle}>
-            <img src={concecionadoIcon}/>
+            <img src={concecionadoIcon} width={"32"} height={"32"}/>
             Concesionado
             </div>
             <div
             style={referenceStyle}>
-            <img src={privadoIcon}/>
+            <img src={privadoIcon} width={"32"} height={"32"}/>
             Privado
             </div>
         </div>
@@ -55,12 +52,15 @@ const legendStyle = {
   position: 'absolute',
   style: 'z-index',
   bottom: '100px',
-  right: '10px',
+  right: '0px',
   background: '#ffffff',
   border: '3px solid',
   margin: '10px',
   padding:'10px',
   dispaly:'block',
+  font: '400 11px Roboto, Arial, sans-serif',
+  alignItems: 'center',
+  justifyContent: 'left',
 };
 
 
